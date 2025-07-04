a713adc8449700da90be125be9aa7a90df34bec3
chri-aagnes <chri.aagnes@gmail.com>
2025-07-04 15:37:55 -0400

    implementing batch data func and cleaning example code

--- Dataloader/dataloader.py
+++ Dataloader/dataloader.py
@@ -3,16 +3,19 @@ Dataloader class (non-thought out template code!!)
 
 X: independent variable stored as a numpy array, shape: (batch size, feature size)
 y: response variable stored as a numpy array, shape: (feature size, )
+batch_size: number of data points used for gradient updates, defaults to 1. Note: the last batch will be dropped.
 '''
 
+import prefetch
 import numpy as np
 
 
 class Dataloader: 
-    def __init__(self, X, y, batch_size, shuffle=False, transformations=None): 
+    def __init__(self, X, y, batch_size=1, prefetch=True, shuffle=False, transformations=None): 
         self.X = X 
         self.y = y.reshape(1, -1)
         self.batch_size = batch_size
+        self.prefetch = prefetch
         self.shuffle = shuffle
         self.transformations = transformations
 
@@ -21,11 +24,13 @@ class Dataloader:
         if transformations: 
             self.X = self.custom_data_transformations()
 
+        self.batch_data()
+        
 
     def shuffle_data(self): 
-        assert X.shape[1] == y.shape[1], "X and y do not have matching dimensions. X should be a 2D numpy array of size (batch size, feature size) while y should be a 1D numpy array of size (feature size, )."
+        assert self.X.shape[1] == self.y.shape[1], "X and y do not have matching dimensions. X should be a 2D numpy array of size (batch size, feature size) while y should be a 1D numpy array of size (feature size, )."
         shuffler = np.random.default_rng()
-        full_data = np.concatenate(self.X, self.y) # make sure (X, y) pairs get shuffled together
+        full_data = np.concatenate((self.X, self.y)) # make sure (X, y) pairs get shuffled together
         shuffler.shuffle(full_data, axis=1)  # shuffles the columns the combined data
         self.X, self.y = full_data[:-1, :], full_data[-1, :]
         return self.X, self.y
@@ -40,28 +45,32 @@ class Dataloader:
 
 
     def batch_data(self): 
-        assert 1 <= self.batch_size <= len(self.X)
-        pass 
+        assert 1 <= self.batch_size <= len(self.X), "The batch size needs to be a positive integer between 1 and the length of the dataset (inclusive)."
+        for start in range(0, len(self.X), self.batch_size): # drops remaining data samples
+            end = start + self.batch_size 
+            X_batch = self.X[start:end]
+            y_batch = self.y[start:end]
+
+            if self.prefetch: # call prefetch.cpp
+                prefetch.process_batch(X_batch, y_batch) 
+                yield X_batch, y_batch
+            else: # store batches without prefetching
+                pass
 
 
 if __name__=="__main__":
     X = np.array([[1, 2, 3, 4, 5], [-10, -20, -30, -40, -50]])
-    y = np.array([15, 25, 35, 45, 55]).reshape(1, -1)
+    y = np.array([15, 25, 35, 45, 55])
     def myfunc(x):
         return x+2
-    #print(np.vectorize(myfunc)(X))
-    print(X.shape, y.shape)
-    full = np.concatenate((X, y))
-    print(full)
-    print("")
-
-    shuffler = np.random.default_rng()
-    shuffler.shuffle(full, axis=1)
-    print(full)
+    
+    test = Dataloader(X, y, batch_size=2, prefetch=True, shuffle=True, transformations=[myfunc])
     
     print("")
-    X, y = full[:-1, :], full[-1, :]
-    print(X, y)
+    print(X)
+    print(test.X, "\n")
 
-    assert callable(myfunc)
-    assert X.shape[1] == y.shape[1] 
+    print(y)
+    print(test.y, "\n")
+
+    #print(test.X_batch)
